8ec4752237deda4b781ce666b953f44fa01fc93b
Dhanashri Patil <patil.dhanashrik@gmail.com>
2026-03-11 00:59:37 +0530

    Add basic operations on Python lists

---
+++ 03_Data_Structures/lists.py
@@ -0,0 +1,21 @@
+# Python Lists
+
+numbers = [1,2,3,4,5]
+
+print(numbers)
+
+# Access element
+print(numbers[0])
+
+# Add element
+numbers.append(6)
+
+# Remove element
+numbers.remove(3)
+
+# Length
+print(len(numbers))
+
+# Loop list
+for num in numbers:
+    print(num)
