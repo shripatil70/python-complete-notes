f3cdb612403f626a94b0cefd3bc99f2ea0c6797f
Dhanashri Patil <patil.dhanashrik@gmail.com>
2026-03-11 01:00:07 +0530

    Add example of dictionary usage in Python

---
+++ 03_Data_Structures/dictionaries.py
@@ -0,0 +1,18 @@
+# Dictionary Example
+
+student = {
+    "name": "Alice",
+    "age": 22,
+    "course": "Computer Science"
+}
+
+print(student)
+
+print(student["name"])
+
+# Add value
+student["city"] = "Pune"
+
+# Keys and values
+print(student.keys())
+print(student.values())
